5ddeca9140f07b55e98345f3b1da30e022fa57ed
Abraham Konchellah <abrahamkonchellah@gmail.com>
2025-04-26 18:18:46 +0000

    Function6 to add the digits of a number

---
+++ Function6.py
@@ -0,0 +1,9 @@
+# recursive function to add the digits of a number using recursion
+def add(n):
+    if n == 0:
+        return 0 #Base case
+    else:
+     return (n % 10) + add(n // 10) #Recursive cas
+
+n=int(input("Enter a long number(e.g 12345:"))
+print(add(n))
