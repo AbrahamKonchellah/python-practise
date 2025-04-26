f0582836b3f2495f6b2a2bac1f5f8422e3c24a0c
Abraham Konchellah <abrahamkonchellah@gmail.com>
2025-04-26 18:39:27 +0000

    Function5 to reverse a string

--- Function6.py
+++ Function6.py
@@ -3,7 +3,7 @@ def add(n):
     if n == 0:
         return 0 #Base case
     else:
-     return (n % 10) + add(n // 10) #Recursive cas
+     return (n % 10) + add(n // 10) #Recursive case
 
 n=int(input("Enter a long number(e.g 12345:"))
 print(add(n))
